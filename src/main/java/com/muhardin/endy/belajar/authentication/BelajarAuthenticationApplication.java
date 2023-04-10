afcbe89e42925a17178d9accf880fb962c1f7848
Endy Muhardin <endy.muhardin@gmail.com>
2023-04-10 10:38:37 +0700

    starter project

---
+++ src/main/java/com/muhardin/endy/belajar/authentication/BelajarAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarAuthenticationApplication.class, args);
+	}
+
+}
