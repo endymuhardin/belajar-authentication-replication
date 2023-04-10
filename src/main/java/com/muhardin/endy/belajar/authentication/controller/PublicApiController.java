b40a369a99f632a6e3ef6ee8962ec3489c5d9964
Endy Muhardin <endy.muhardin@gmail.com>
2023-04-10 10:55:33 +0700

    implement git version

--- src/main/java/com/muhardin/endy/belajar/authentication/controller/PublicApiController.java
+++ src/main/java/com/muhardin/endy/belajar/authentication/controller/PublicApiController.java
@@ -3,6 +3,8 @@ package com.muhardin.endy.belajar.authentication.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +13,14 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 public class PublicApiController {
 
+    @Autowired private GitProperties gitProperties;
+
     @GetMapping("/api/public/appinfo")
     public Map<String, String> appinfo(HttpServletRequest request){
         Map<String, String> info = new HashMap<>();
         info.put("ip-server", request.getLocalAddr());
         info.put("ip-client", request.getRemoteAddr());
+        info.put("app-version", gitProperties.getShortCommitId());
         return info;
     }
 }
